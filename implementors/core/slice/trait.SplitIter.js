c5098cbebbcc475c94d927af16fc5fe0b402c39e
Manish Goregaokar <manishsmail@gmail.com>
2015-06-05 22:51:34 +0530

    Compiler docs for 19a39710c494d9300ffdc4e3f40dfda0ef62f7ba ( Fri Jun 5 11:59:34 2015 +0000 )

--- implementors/core/slice/trait.SplitIter.js
+++ implementors/core/slice/trait.SplitIter.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['collections'] = [];implementors['rustc_unicode'] = [];
+implementors['collections'] = [];implementors['std'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
