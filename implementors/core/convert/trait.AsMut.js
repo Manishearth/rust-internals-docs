c2925f8fd9cc0c22b9410533d2194902850fa7c8
Manish Goregaokar <manishsmail@gmail.com>
2015-06-22 18:44:36 +0530

    Compiler docs for 2287b4b628cb6f4d66578e1298cd9f34e9ef77db (Mon Jun 22 05:23:50 2015 +0000)

--- implementors/core/convert/trait.AsMut.js
+++ implementors/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['collections'] = [];implementors['core'] = [];implementors['rustc_unicode'] = [];
+implementors['collections'] = [];implementors['rustc_unicode'] = [];implementors['core'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
