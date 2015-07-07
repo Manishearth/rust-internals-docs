6d04b331a446d9318a314fa2a1d3f3ee9f941bf5
Manish Goregaokar <manishsmail@gmail.com>
2015-07-07 22:40:40 +0530

    Compiler docs for 26f0cd5de7f71a0db0bb3857ce49a11cd0f7d876 (Tue Jul 7 02:26:31 2015 +0000)

--- implementors/core/str/pattern/trait.TwoWayStrategy.js
+++ implementors/core/str/pattern/trait.TwoWayStrategy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['std'] = [];implementors['collections'] = [];implementors['rustc_unicode'] = [];implementors['rustc'] = [];implementors['rustc_borrowck'] = [];implementors['rustc_typeck'] = [];implementors['rustc_trans'] = [];implementors['rustc_lint'] = [];implementors['rustdoc'] = [];
+implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['collections'] = [];implementors['rustc'] = [];implementors['rustc_borrowck'] = [];implementors['rustc_typeck'] = [];implementors['rustc_lint'] = [];implementors['rustc_trans'] = [];implementors['rustdoc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
