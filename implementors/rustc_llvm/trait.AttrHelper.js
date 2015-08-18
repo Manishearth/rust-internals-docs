ace0b16428df60d799995744135eeeaa5049f83a
Manish Goregaokar <manishsmail@gmail.com>
2015-08-19 03:20:24 +0530

    Compiler docs for 6c11e4a48ec3ac385f32da1879384bc6921bdf75 (Tue Aug 18 18:41:17 2015 +0000)

--- implementors/rustc_llvm/trait.AttrHelper.js
+++ implementors/rustc_llvm/trait.AttrHelper.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc_llvm'] = [];implementors['rustc_borrowck'] = [];implementors['rustc_typeck'] = [];implementors['rustc_lint'] = [];implementors['rustdoc'] = [];
+implementors['rustc_llvm'] = [];implementors['rustc_borrowck'] = [];implementors['rustc_lint'] = [];implementors['rustc_typeck'] = [];implementors['rustdoc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
