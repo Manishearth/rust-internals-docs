7be1b91b3469bfb289e79053b1702b2602b55b04
Manish Goregaokar <manishsmail@gmail.com>
2015-10-30 02:15:06 +0530

    Compiler docs for a18e0b27077c763df4f9e06b42192717025f0bfc (Thu Oct 29 16:03:46 2015 +0000)

--- implementors/std/ops/trait.Add.js
+++ implementors/std/ops/trait.Add.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['std'] = ["impl&lt;'a&gt; <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a>&lt;&amp;'a <a href='primitive.str.html'>str</a>&gt; for <a class='struct' href='std/string/struct.String.html' title='std::string::String'>String</a>","impl <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a> for <a class='struct' href='std/time/struct.Duration.html' title='std::time::Duration'>Duration</a>",];
+implementors['std'] = ["impl&lt;'a&gt; <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a>&lt;&amp;'a <a class='primitive' href='primitive.str.html'>str</a>&gt; for <a class='struct' href='std/string/struct.String.html' title='std::string::String'>String</a>","impl <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a> for <a class='struct' href='std/time/struct.Duration.html' title='std::time::Duration'>Duration</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
