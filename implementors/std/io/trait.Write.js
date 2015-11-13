cee494d8a5c462ce28bde62d0d16978867a0a2b1
Manish Goregaokar <manishsmail@gmail.com>
2015-11-13 14:38:43 +0530

    Compiler docs for afd4a5420c3a9f6642da655543aa26df15b2ef9c (Thu Nov 12 08:02:43 2015 +0000)

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['std'] = [];implementors['rustc_trans'] = ["impl <a class='trait' href='std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='enum' href='syntax/diagnostic/enum.Destination.html' title='syntax::diagnostic::Destination'>Destination</a>",];implementors['rustc_driver'] = ["impl <a class='trait' href='std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='enum' href='syntax/diagnostic/enum.Destination.html' title='syntax::diagnostic::Destination'>Destination</a>",];
+implementors['std'] = [];implementors['rustc_driver'] = ["impl <a class='trait' href='std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='enum' href='syntax/diagnostic/enum.Destination.html' title='syntax::diagnostic::Destination'>Destination</a>",];implementors['rustc_trans'] = ["impl <a class='trait' href='std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='enum' href='syntax/diagnostic/enum.Destination.html' title='syntax::diagnostic::Destination'>Destination</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/syntax/print/pprust/trait.PrintState.js
+++ implementors/syntax/print/pprust/trait.PrintState.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['syntax'] = [];implementors['rustc_front'] = ["impl&lt;'a&gt; <a class='trait' href='syntax/print/pprust/trait.PrintState.html' title='syntax::print::pprust::PrintState'>PrintState</a>&lt;'a&gt; for <a class='struct' href='rustc_front/print/pprust/struct.State.html' title='rustc_front::print::pprust::State'>State</a>&lt;'a&gt;",];implementors['rustc_trans'] = [];implementors['rustc_driver'] = [];
+implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['rustc_front'] = ["impl&lt;'a&gt; <a class='trait' href='syntax/print/pprust/trait.PrintState.html' title='syntax::print::pprust::PrintState'>PrintState</a>&lt;'a&gt; for <a class='struct' href='rustc_front/print/pprust/struct.State.html' title='rustc_front::print::pprust::State'>State</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
