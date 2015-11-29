192a813b4ae1784a6ff13e3ba33a1bff22c4a3c7
Manish Goregaokar <manishsmail@gmail.com>
2015-11-29 12:35:36 +0530

    Compiler docs for 7499558dd0a64b3485f2d1b3c7df05616893673d (Sat Nov 28 19:57:45 2015 +0000)

--- syntax/util/sidebar-items.js
+++ syntax/util/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["interner","An \"interner\" is a data structure that associates values with usize tags and allows bidirectional lookup; i.e. given a value, one can easily find the type, and vice versa."],["node_count",""],["parser",""],["small_vector",""]]});
+initSidebarItems({"mod":[["interner","An \"interner\" is a data structure that associates values with usize tags and allows bidirectional lookup; i.e. given a value, one can easily find the type, and vice versa."],["lev_distance",""],["node_count",""],["parser",""],["small_vector",""]]});
