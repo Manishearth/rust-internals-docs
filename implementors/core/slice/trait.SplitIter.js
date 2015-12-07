cad319c1c60a67a50a3728c27a3592bdf3391e59
Manish Goregaokar <manishsmail@gmail.com>
2015-12-07 08:06:54 -0500

    Compiler docs for 7b77f67d1942620d550f0067c92d781c623aef58 (Mon Dec 7 07:11:19 2015 +0000)

--- implementors/core/slice/trait.SplitIter.js
+++ implementors/core/slice/trait.SplitIter.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['collections'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];
+implementors['collections'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];implementors['std'] = [];implementors['rustc_unicode'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
