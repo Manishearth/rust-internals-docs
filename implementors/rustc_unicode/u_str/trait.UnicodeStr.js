58a725f8b74b822f302821c74bd511d1d4f86058
Manish Goregaokar <manishsmail@gmail.com>
2016-02-23 12:49:46 +0530

    Compiler docs for 37c6f2881c634fe72dc98b846037dc1237270659 (Mon Feb 22 23:13:08 2016 +0000)

--- implementors/rustc_unicode/u_str/trait.UnicodeStr.js
+++ implementors/rustc_unicode/u_str/trait.UnicodeStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['std'] = [];implementors['collections'] = [];
+implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
