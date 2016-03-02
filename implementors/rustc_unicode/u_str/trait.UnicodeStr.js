db7b8f4072dbd27ffb814fe0861fa6a585bac8b6
Manish Goregaokar <manishsmail@gmail.com>
2016-03-02 11:21:12 +0530

    Compiler docs for 0400d929e8626192ba97b4ec12e52aabc8b476d5 (Wed Mar 2 02:40:07 2016 +0000)

--- implementors/rustc_unicode/u_str/trait.UnicodeStr.js
+++ implementors/rustc_unicode/u_str/trait.UnicodeStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];
+implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];implementors['collections'] = [];implementors['std'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
