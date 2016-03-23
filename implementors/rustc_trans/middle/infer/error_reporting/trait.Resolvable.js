6adbe4f1ce86d8676a848d9609da9d2e5255438e
Manish Goregaokar <manishsmail@gmail.com>
2016-03-23 18:26:16 +0530

    Compiler docs for d6af19b89c477708ed0f5341ab7a26f1764f1ffa (Wed Mar 23 01:03:04 2016 -0700)

--- implementors/rustc_trans/middle/infer/error_reporting/trait.Resolvable.js
+++ implementors/rustc_trans/middle/infer/error_reporting/trait.Resolvable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];
+implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];implementors['rustc_trans'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/syntax/ext/quote/rt/trait.ToTokens.js
+++ implementors/syntax/ext/quote/rt/trait.ToTokens.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];
+implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];implementors['syntax'] = [];implementors['rustc_trans'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
