f4a6bc66863775f2463113d3d069e0df87ef10d1
Manish Goregaokar <manishsmail@gmail.com>
2016-03-30 06:11:52 +0530

    Compiler docs for 8f5c3f1fcf77ec890d340dc3beb676f2a01ae99c (Tue Mar 29 11:10:39 2016 -0700)

--- implementors/rustdoc/fold/trait.DocFolder.js
+++ implementors/rustdoc/fold/trait.DocFolder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];
+implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];implementors['rustdoc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
