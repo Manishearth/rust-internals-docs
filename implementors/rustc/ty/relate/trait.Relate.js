80a0c17f000401d6d69081bd25d386e782ef510c
Manish Goregaokar <manishsmail@gmail.com>
2016-04-12 12:16:03 +0530

    Compiler docs for c0221c8897db309a79990367476177b1230bb264 (Mon Apr 11 13:20:56 2016 -0700)

--- implementors/rustc/ty/relate/trait.Relate.js
+++ implementors/rustc/ty/relate/trait.Relate.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc_typeck'] = [];implementors['rustc'] = [];implementors['rustc_typeck'] = [];implementors['rustc'] = [];implementors['rustc_typeck'] = [];
+implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc_typeck'] = [];implementors['rustc'] = [];implementors['rustc_typeck'] = [];implementors['rustc'] = [];implementors['rustc_typeck'] = [];implementors['rustc'] = [];implementors['rustc_typeck'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
