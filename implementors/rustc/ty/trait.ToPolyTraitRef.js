a2525213133f4fbc030e272fa08df768368ac7fb
Manish Goregaokar <manishsmail@gmail.com>
2016-04-27 18:12:42 +0530

    Compiler docs for 80bff1eea750404e559887d435c03d21f2b25921 (Tue Apr 26 22:47:40 2016 -0700)

--- implementors/rustc/ty/trait.ToPolyTraitRef.js
+++ implementors/rustc/ty/trait.ToPolyTraitRef.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];
+implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc'] = [];implementors['rustc_metadata'] = [];implementors['rustc_typeck'] = [];implementors['rustc_lint'] = [];implementors['rustc_borrowck'] = [];implementors['rustc_trans'] = [];implementors['rustdoc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
