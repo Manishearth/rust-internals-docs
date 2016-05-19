cc84f164805cdc540419cd88abe17542ff7622f8
Manish Goregaokar <manishsmail@gmail.com>
2016-05-19 22:42:47 +0530

    Compiler docs for d0ca0ca0631eb7105c51b1dc4153594d4b9dfbfd (Thu May 19 01:50:53 2016 -0700)

--- implementors/rustc_save_analysis/trait.Dump.js
+++ implementors/rustc_save_analysis/trait.Dump.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc_save_analysis'] = [];implementors['rustc_save_analysis'] = [];implementors['rustc_save_analysis'] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];
+implementors['rustc_save_analysis'] = [];implementors['rustc_save_analysis'] = [];implementors['rustc_save_analysis'] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];implementors["rustc_save_analysis"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/test/stats/trait.Stats.js
+++ implementors/test/stats/trait.Stats.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['test'] = [];implementors['test'] = [];implementors['test'] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];
+implementors['test'] = [];implementors['test'] = [];implementors['test'] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];implementors["test"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
