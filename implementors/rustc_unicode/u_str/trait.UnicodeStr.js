a97ee2fa2bcfb373391a05f47852dd04d546ed8e
Manish Goregaokar <manishsmail@gmail.com>
2016-06-20 13:58:38 +0530

    Compiler docs for fa4cf7b26076a7134a9f000911c0b3d3642876bd (Sun Jun 19 17:22:54 2016 -0700)

--- implementors/rustc_unicode/u_str/trait.UnicodeStr.js
+++ implementors/rustc_unicode/u_str/trait.UnicodeStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["collections"] = [];
+implementors["collections"] = [];implementors["std"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
