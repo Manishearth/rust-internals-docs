d6f8cfebb3bf315fdd09491d2f99f596947f29cd
Manish Goregaokar <manishsmail@gmail.com>
2016-08-06 20:56:59 +0530

    Compiler docs for ecdd51b7bb7fd993acd2ff5dbd72209244b1e4aa (Fri Aug 5 23:28:29 2016 -0700)

---
+++ implementors/rustc_resolve/trait.ToNameBinding.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rustc_resolve"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
