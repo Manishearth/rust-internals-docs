e1719463828cf53c4887effc335c03648414e8a1
Manish Goregaokar <manishsmail@gmail.com>
2016-08-13 21:46:36 +0530

    Compiler docs for e64f68817d850ccbe642d7f067083bc655115d84 (Sat Aug 13 01:20:46 2016 -0700)

--- syntax/ext/expand/sidebar-items.js
+++ syntax/ext/expand/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["expand_crate",""],["expand_expr",""],["expand_item",""],["expand_type",""]],"struct":[["ExpansionConfig",""],["MacroExpander","A tree-folder that performs macro expansion"]]});
+initSidebarItems({"fn":[["expand_crate",""],["expand_crate_with_expander",""],["expand_expr",""],["expand_item",""],["expand_type",""]],"struct":[["ExpansionConfig",""],["MacroExpander","A tree-folder that performs macro expansion"]]});
