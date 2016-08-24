24d793566134095ce6a3b58ac38794f35ba6aaf0
Manish Goregaokar <manishsmail@gmail.com>
2016-08-24 15:24:43 +0530

    Compiler docs for 308824acecf902f2b6a9c1538bde0324804ba68e (Tue Aug 23 22:52:29 2016 -0700)

---
+++ implementors/core/ops/trait.Carrier.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["collections"] = [];implementors["rustc_unicode"] = [];implementors["rustc_data_structures"] = [];implementors["core"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
