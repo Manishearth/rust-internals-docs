cd095c34af9c15d659ecf877c938336d37b486fe
Manish Goregaokar <manishsmail@gmail.com>
2016-10-19 09:16:01 +0530

    Compiler docs for 16eeeac783d2ede28e09f2a433c612dea309fe33 (Tue Oct 18 05:09:12 2016 -0700)

--- rustc_lint/session/filesearch/sidebar-items.js
+++ rustc_lint/session/filesearch/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["FileMatch",""]],"fn":[["get_or_default_sysroot",""],["relative_target_lib_path",""],["rustlibdir",""]],"struct":[["FileSearch",""]]});
+initSidebarItems({"enum":[["FileMatch",""]],"fn":[["get_or_default_sysroot",""],["relative_target_lib_path",""]],"struct":[["FileSearch",""]]});
