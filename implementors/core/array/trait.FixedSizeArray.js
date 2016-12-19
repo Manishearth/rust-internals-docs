f85774130e3cb28eeda619e08715bfd65a9127f9
Manish Goregaokar <manishsmail@gmail.com>
2016-12-19 03:41:54 -0800

    Compiler docs for cc662efca1e2a070887839a935c3d14de4991526 (Mon Dec 19 04:39:18 2016 +0000)

--- implementors/core/array/trait.FixedSizeArray.js
+++ implementors/core/array/trait.FixedSizeArray.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["collections"] = [];implementors["std"] = [];implementors["rustc_unicode"] = [];implementors["rustc_data_structures"] = [];implementors["core"] = [];implementors["syntax"] = [];implementors["rustc"] = [];implementors["rustc_trans"] = [];implementors["rustc_lint"] = [];implementors["rustc_metadata"] = [];implementors["rustc_mir"] = [];implementors["rustc_typeck"] = [];implementors["rustc_borrowck"] = [];implementors["rustdoc"] = [];
+implementors["collections"] = [];implementors["std"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/test/trait.FnBox.js
+++ implementors/test/trait.FnBox.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["test"] = [];implementors["rustdoc"] = [];
+implementors["test"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
