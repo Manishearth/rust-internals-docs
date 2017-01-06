14b4ddc4865ce7397c3a68e986caadd07c2f145f
Manish Goregaokar <manishsmail@gmail.com>
2017-01-06 11:06:14 -0800

    Compiler docs for e92bc9162c2060205844dbceeb7718f985753e09 (Fri Jan 6 10:18:44 2017 -0800)

--- implementors/collections/string/trait.ToString.js
+++ implementors/collections/string/trait.ToString.js
@@ -1,14 +1,5 @@
 (function() {var implementors = {};
-implementors["rustc"] = [];
-implementors["rustc_borrowck"] = [];
-implementors["rustc_data_structures"] = [];
-implementors["rustc_lint"] = [];
-implementors["rustc_metadata"] = [];
-implementors["rustc_mir"] = [];
-implementors["rustc_trans"] = [];
-implementors["rustc_typeck"] = [];
-implementors["rustdoc"] = [];
-implementors["syntax"] = [];
+implementors["collections"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
